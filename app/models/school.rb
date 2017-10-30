33c934c488d1556da9ea5633ab3ec6a13d25889b
dho <vidanho@gmail.com>
2017-10-30 16:48:42 +0000

    Added foreign key for students to school relationship. New db entries.

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,4 @@
 class School < ApplicationRecord
+  has_many :students # This is a foreign key of school_id for Students.
   validates :name, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,5 @@
 class Student < ApplicationRecord
+  belongs_to :school # There is a school_id int key in the schools table. This is a foreign key for Students.
+
   validates :name, :student_number, presence:true
 end
